47cd26e26dd26d41c067d55bd9c70a327f055ac1
mezcel <mezcel@hotmail.com>
2020-04-05 03:00:15 -0400

    added prompts

--- powershell_compile.ps1
+++ powershell_compile.ps1
@@ -2,17 +2,82 @@
 ## This will compile and build the cli terminal app on Win10 using Powershell.
 ## Must have Visual Studio installed with C++ in order to compile.
 
-## clean previous builds
-Remove-Item *.obj
-Remove-Item *.exe
+function makeClean {
+    Write-Host "Cleaning ..."
+    ## clean previous builds
+    if (Test-Path *.obj) {
+        Write-Host ""
+        Write-Host "Removing the following:"
+        Get-ChildItem *.obj
+        Remove-Item *.obj
+    }
 
-## Compile and build object modules
-cl sources\my_calendar.c
-cl sources\my_csv_structs.c
-cl sources\my_tty_ui.c
+    if (Test-Path *.exe) {
+        Write-Host ""
+        Write-Host "Removing the following:"
+        Get-ChildItem *.exe
+        Remove-Item *.exe
+    }
+    Write-Host "Done."
+}
 
-## Build the executable file
-cl mainTTY.c my_calendar.obj my_csv_structs.obj my_tty_ui.obj
+function makeBuild {
+    Write-Host "Building ..."
+    ## Compile and build object modules
+    cl sources\my_calendar.c
+    cl sources\my_csv_structs.c
+    cl sources\my_tty_ui.c
 
-## Run
-.\mainTTY.exe
+    ## Build the executable file
+    cl mainTTY.c my_calendar.obj my_csv_structs.obj my_tty_ui.obj
+    Write-Host "Done."
+}
+
+function makeRun {
+    ## Run
+    .\mainTTY.exe
+    Write-Host "Done."
+}
+
+function menu {
+    Write-Host "
+    This script is a DIY Makefile. Select from the folllowing option.
+        clean, build, run
+
+    Options:
+        clean:
+            clear previous builds
+        build:
+            clear previous builds
+            compile and build
+        run:
+            clear previous builds
+            compile and build
+            run the compile executable 'mainTTY.exe'
+    "
+    $userinput = Read-Host -Prompt "Input selection"
+
+    if ( $userinput -eq "clean" ) {
+        Write-Host "You entered: $userinput"
+        makeClean
+    } elseif ( $userinput -eq "build" ) {
+        Write-Host "You entered $userinput"
+        makeClean
+        makeBuild
+    } elseif ( $userinput -eq "run" ) {
+        Write-Host "You entered: $userinput"
+        makeClean
+        makeBuild
+        makeRun
+    } else {
+        Write-Host "You entered: $userinput"
+        Write-Host "    !!! bad input !!!
+            Input only one of the following: 'clean', 'build', or 'run'.
+        Try Again.
+        "
+        Write-Host "Done."
+    }
+
+}
+
+menu
