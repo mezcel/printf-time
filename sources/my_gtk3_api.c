908c5eefc6ce5f14e694ea4f42457e91bae87137
mezcel <mezcel@hotmail.com>
2020-04-17 10:47:42 -0400

    rm redundant void

--- sources/my_gtk3_api.c
+++ sources/my_gtk3_api.c
@@ -14,106 +14,14 @@
  * Header API Scope Functions
  * */
 
-void intializeCalendar( struct tm todaysDate, int *seasonFlag, int *feastFlag );	// local prototype
-void intializeCalendar( struct tm todaysDate, int *seasonFlag, int *feastFlag ) { 
-	struct tm advent_start = setSpecificDate( todaysDate.tm_year + 1900, 11, 1 );					// Dec 1
-	shiftTowardSunday( &advent_start );																// first sun of advent
+void initializeLabelPointers( GtkBuilder *builder, GtkWidget *window, app_widgets *widgets, char *userDefinedFeast ) {
 
-	struct tm immaculate_conception_mary	= setSpecificDate( todaysDate.tm_year + 1900, 11, 8 );	// Dec 8
-	struct tm christmas_day					= setSpecificDate( todaysDate.tm_year + 1900, 11, 25 );	// wed dec 25
-	struct tm solemnity_of_mary				= setSpecificDate( todaysDate.tm_year + 1900, 0, 1 );	// Jan 1
-	struct tm epiphany						= addDays( christmas_day,12 );							// 12 days after christmas
-	shiftTowardSunday( &epiphany );																	// epiphany sunday
-
-	struct tm jesus_baptism					= addDays( christmas_day,12 );
-	shiftJesusBaptism( &jesus_baptism );															// avoid epiphany overlap
-
-	struct tm easter_sunday					= setEasterDate( todaysDate.tm_year + 1900 );			// pfm
-	struct tm good_saturday					= subtractDays( easter_sunday,1 );
-	struct tm good_friday					= subtractDays( easter_sunday,2 );
-	struct tm holy_thursday					= subtractDays( easter_sunday,3 );
-	struct tm ash_wednesday					= subtractDays( easter_sunday,46 );
-	struct tm pentacost						= addDays( easter_sunday,21 );
-	shiftTowardSunday( &pentacost ); 																// 7 sundays after easter
-
-	struct tm assension_of_jesus			= addDays( easter_sunday,40 );
-
-	struct tm all_saints_day				= setSpecificDate( todaysDate.tm_year + 1900, 10, 1 );	// Nov 1
-
-	*seasonFlag = returnLiturgicalSeason( &todaysDate, &advent_start, &christmas_day,
-			&epiphany, &ash_wednesday, &easter_sunday, &pentacost );
-
-	int isFeast;
-	switch ( *seasonFlag ) { 
-		case 0:
-			isFeast = isFeastDay( todaysDate, advent_start );
-			if( isFeast ) { feastFlag = 0; break; }
-
-			isFeast = isFeastDay( todaysDate, immaculate_conception_mary );
-			if( isFeast ) { *feastFlag = 1; break; }
-
-			*feastFlag = 15; // generic seasonal day
-			break;
-		case 1:
-			isFeast = isFeastDay( todaysDate, christmas_day );
-			if( isFeast ) { *feastFlag = 2; break; }
-
-			isFeast = isFeastDay( todaysDate, solemnity_of_mary );
-			if( isFeast ) { *feastFlag = 3; break; }
-
-			isFeast = isFeastDay( todaysDate, epiphany );
-			if( isFeast ) { *feastFlag = 4; break; }
-
-			isFeast = isFeastDay( todaysDate, jesus_baptism );
-			if( isFeast ) { *feastFlag = 5; break; }
-
-			*feastFlag = 15; // generic seasonal day
-			break;
-		case 2:
-			isFeast = isFeastDay( todaysDate, ash_wednesday );
-			if( isFeast ) { *feastFlag = 6; break; }
-
-			isFeast = isFeastDay( todaysDate, holy_thursday );
-			if( isFeast ) { *feastFlag = 7; break; }
-
-			isFeast = isFeastDay( todaysDate, good_friday );
-			if( isFeast ) { *feastFlag = 8; break; }
-
-			isFeast = isFeastDay( todaysDate, good_saturday );
-			if( isFeast ) { *feastFlag = 9; break; }
-
-			isFeast = isFeastDay( todaysDate, easter_sunday );
-			if( isFeast ) { *feastFlag = 10; break; }
-
-			*feastFlag = 15; // generic seasonal day
-			break;
-		case 3:
-			isFeast = isFeastDay( todaysDate, pentacost );
-			if( isFeast ) { *feastFlag = 11; break; }
-
-			isFeast = isFeastDay( todaysDate, assension_of_jesus );
-			if( isFeast ) { *feastFlag = 12; break; }
-
-			*feastFlag = 15; // generic seasonal day
-			break;
-		default:
-			isFeast = isFeastDay( todaysDate, all_saints_day );
-			if( isFeast ) { *feastFlag = 13; break; }
-			if( isFeast == 0 ) { *feastFlag = 14; break; }
-			break;
-	}
-}
-
-void initializeLabelPointers( GtkBuilder *builder, GtkWidget *window, app_widgets *widgets, char *userDefinedFeast ) { 
-
-	struct tm todaysDate = returnTodaysDate();
-	int seasonFlag = 4; // ordinary time
-	int feastFlag = 14; // ordinary day
-
-	intializeCalendar( todaysDate, &seasonFlag, &feastFlag );
+	struct tm todaysDate	= returnTodaysDate();
+	int seasonFlag			= 4; // ordinary time
+	int feastFlag			= 14; // ordinary day
 
 	// labels
-	widgets -> lblTextDate = GTK_WIDGET( gtk_builder_get_object( builder, "lblTextDate" ) );
+	widgets -> lblTextDate				= GTK_WIDGET( gtk_builder_get_object( builder, "lblTextDate" ) );
 
 	widgets -> lblTextMystery			= GTK_WIDGET( gtk_builder_get_object( builder, "lblTextMystery" ) );
 	widgets -> lblTextDecade			= GTK_WIDGET( gtk_builder_get_object( builder, "lblTextDecade" ) );
@@ -147,17 +55,17 @@ void initializeLabelPointers( GtkBuilder *builder, GtkWidget *window, app_widget
 	}
 }
 
-int initialMystery( int weekdayNo ) { 
-	int navigtionPosition[ ] = { 237, 0, 158, 237, 79, 158, 0 };
+int initialMystery( int weekdayNo ) {
+	int navigtionPosition[] = { 237, 0, 158, 237, 79, 158, 0 };
 
-	if ( ( weekdayNo > 6 ) || ( weekdayNo < 0 ) ) { 
+	if ( ( weekdayNo > 6 ) || ( weekdayNo < 0 ) ) {
 		weekdayNo = 0;
 	}
 
 	return navigtionPosition[ weekdayNo ];
 }
 
-void update_widgets_labels( rosary_db_t *rosary_db_struct, app_widgets *widgets ) { 
+void update_widgets_labels( rosary_db_t *rosary_db_struct, app_widgets *widgets ) {
 	int navigtionPosition	= widgets->navigtionPosition;
 
 	/*
@@ -190,35 +98,35 @@ void update_widgets_labels( rosary_db_t *rosary_db_struct, app_widgets *widgets 
 	int sectionWhole;
 	char *progressLabel;
 
-	if ( loopBody == 1 ) { 
+	if ( loopBody == 1 ) {
 		sectionWhole	= 10;
 		smallbeadDouble = ( double )smallbeadPercent / ( double )sectionWhole;
 		progressLabel	= "rosary body";
-	} else { 
-		if( ( prayerFK == 7 ) || ( prayerFK == 8 ) ) { 
+	} else {
+		if( ( prayerFK == 7 ) || ( prayerFK == 8 ) ) {
 		sectionWhole	= 10; // not really, it is a placeholder
 		smallbeadDouble = ( double )smallbeadPercent / ( double )sectionWhole;
 		progressLabel	= "conclusion prayers";
-	} else { 
+	} else {
 		sectionWhole = 7;
 		smallbeadDouble = ( double )smallbeadPercent / ( double )sectionWhole;
 		progressLabel	= "introduction prayers";
 	}
 	}
 
-	if ( smallbeadDouble < 0.01 ) { 
+	if ( smallbeadDouble < 0.01 ) {
 		smallbeadDouble = 0.00;
 	}
-	if ( smallbeadDouble > 1.00 ) { 
+	if ( smallbeadDouble > 1.00 ) {
 		smallbeadDouble = 1.00;
 	}
 	double smallbeadPercentDouble = smallbeadDouble * 100.0;
 
 	double mysteryDouble = ( double )mysteryPercent / 50.0;
-	if ( mysteryDouble < 0.01 ) { 
+	if ( mysteryDouble < 0.01 ) {
 		mysteryDouble = 0.00;
 	}
-	if ( mysteryDouble > 1.00 ) { 
+	if ( mysteryDouble > 1.00 ) {
 		mysteryDouble = 1.00;
 	}
 	double mysteryPercentDouble = mysteryDouble * 100.0;
@@ -270,6 +178,7 @@ void update_widgets_labels( rosary_db_t *rosary_db_struct, app_widgets *widgets 
 	gtk_level_bar_set_value( GTK_LEVEL_BAR( widgets -> levelBar_mystery ), mysteryDouble );
 }
 
+/*
 void updateFeastDisplayStruct( feast_db_t *feast_db_struct, displayFeastVariables_t *queryFeastViewStruct, int todayDay, int todayMonth ) {
 	int feastDay	= 0,
 		feastMonth	= 0,
@@ -312,6 +221,61 @@ void updateFeastDisplayStruct( feast_db_t *feast_db_struct, displayFeastVariable
 		strcpy( queryFeastViewStruct -> feastName, "" );
 		strcpy( queryFeastViewStruct -> monthName, "" );
 
+	}
+
+}
+*/
+
+void updateFeastDisplayStruct( feast_db_t *feast_db_struct, displayFeastVariables_t *queryFeastViewStruct, int todayDay, int todayMonth, int recordCount ) {
+	int feastDay	= 0,
+		feastMonth	= 0,
+		counter		= 0;
+	int feastID,
+		isFeastDay	= 0; // flag if today's date is a user defined feast date
+
+	char *feastName, *monthName;
+
+	while ( counter < recordCount  ) {
+		feastDay	= feast_db_struct -> feast_dbArray[ counter ].feastDay;
+		feastMonth	= feast_db_struct -> feast_dbArray[ counter ].feastMonth;
+
+		if ( todayDay == feastDay ) {
+			if ( todayMonth == feastMonth ) {
+				isFeastDay = 1;
+				break;
+			}
+		}
+
+		counter++;
+	}
+
+	if ( isFeastDay ) {
+		feastID		= feast_db_struct -> feast_dbArray[ counter ].feastID;
+		feastDay	= feast_db_struct -> feast_dbArray[ counter ].feastDay;
+		feastMonth	= feast_db_struct -> feast_dbArray[ counter ].feastMonth;
+
+		feastName	= feast_db_struct -> feast_dbArray[ counter ].feastName;
+		monthName	= feast_db_struct -> feast_dbArray[ counter ].monthName;
+
+		// int's
+		queryFeastViewStruct -> feastID		= feastID;
+		queryFeastViewStruct -> feastDay	= feastDay;
+		queryFeastViewStruct -> feastMonth	= feastMonth;
+
+		// strings
+		strcpy( queryFeastViewStruct -> feastName, feastName );
+		strcpy( queryFeastViewStruct -> monthName, monthName );
+	} else {
+		// Populate queryFeastViewStruct with dummy values
+
+		// int's
+		queryFeastViewStruct -> feastID		= 0;
+		queryFeastViewStruct -> feastDay	= 0;
+		queryFeastViewStruct -> feastMonth	= 0;
+
+		// strings
+		strcpy( queryFeastViewStruct -> feastName, "" );
+		strcpy( queryFeastViewStruct -> monthName, "" );
 	}
 
 }
