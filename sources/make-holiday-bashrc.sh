7157d92f6d307a9e35c8a26c5489cae7e41a115e
mezcel <mezcel@hotmail.com>
2021-04-15 05:05:46 -0500

    added cygwin alias

--- sources/make-holiday-bashrc.sh
+++ sources/make-holiday-bashrc.sh
@@ -1,13 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 uname -a | grep "GNU" &>/dev/null
 isDebian=$?
 
+uname -a | grep "CYGWIN" &>/dev/null
+isCygwin=$?
+
+isUnixPass=1
+
 if [ $isDebian -eq 0 ]; then
+    ## Debian Linux or WSL Debian
+    echo -e "#\tDebian detected ..."
+    isUnixPass=0
+elif [ $isCygwin -eq 0 ]; then
+    ## CYGWIN Gnu
+    echo -e "#\tCygwin detected ..."
+    isUnixPass=0
+else
+    echo "#\tDebian and Cygwin is not detected."
+fi
+
+if [ $isUnixPass -eq 0 ]; then
     command -v bash &>/dev/null
     isBash=$?
 
     if [ $isBash -eq 0 ]; then
+        echo -e "#\tBash Detected ..."
+
         origianlBashrc=~/.bashrc
         backupBashrc=$origianlBashrc.backup_$(date +%d%b%Y%H%S)
         tempBashrc=$origianlBashrc.temp
@@ -31,15 +50,27 @@ if [ $isDebian -eq 0 ]; then
         #echo -e "currentDir=\$(pwd); cd $projectRoot; ./ttyBashrc; cd \$currentDir" >> $tempBashrc
         #echo -e "exec $(pwd)/ttyBashrc -d $(pwd)/" >> $tempBashrc
         #echo -e "ttyBashrc -d $(pwd)/database/" >> $tempBashrc
-        echo -e "alias ttyBashrc=\"ttyBashrc -d $(pwd)/database/\"\nttyBashrc" >> $tempBashrc
+		
+		if [ $isCygwin -eq 0 ]; then
+	        echo -e "alias ttyBashrc=\"ttyBashrc -d $(pwd)/database/\"\nttyBashrc.exe" >> $tempBashrc
+		else
+	        echo -e "alias ttyBashrc=\"ttyBashrc -d $(pwd)/database/\"\nttyBashrc" >> $tempBashrc
+		fi
 
         mv $tempBashrc $origianlBashrc
         echo -e "#\tAppended $(pwd)/ttyBashrc to ~/.bashrc ..."
 
 
         #sudo ln -s ttyBashrc /usr/bin/ttyBashrc
+		
+		command -v sudo &>/dev/null
+		isSudo=$?
+		if [ $isSudo -eq 0 ]; then
+			sudo cp ttyBashrc /usr/bin/
+		fi
 
-        sudo cp ttyBashrc /usr/bin/
-        echo -e "#\tMade global user /usr/bin/ttyBashrc copy..."
+		cp ttyBashrc /usr/bin/
+		echo -e "#\tMade global user /usr/bin/ttyBashrc copy..."
+
     fi
 fi
